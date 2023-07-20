e8473d7a4ff6181bd8343bd70abc12eefaea194e
Fiat_Anw <fiat.anw@gmail.com>
2023-07-20 22:28:03 +0700

    finish Management page

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import { Box, Flex } from "@chakra-ui/react";
 
 const RootLayout = () => {
   return (
--- frontend/src/pages/Management/index.jsx
+++ frontend/src/pages/Management/index.jsx
@@ -1,5 +1,31 @@
+import Content from './ManagementContent'
+import { ContextProvider } from './context/ManagementContext';
+import { Box, Center, Flex, Grid, GridItem, Spacer, Text } from "@chakra-ui/react";
+
 const Management = () => {
-  return <div>Management</div>;
+
+  return (
+    <ContextProvider>
+      <Grid 
+        h={"100%"}
+        mt={16}
+        mx={5}
+        templateAreas={`
+          "title"
+          "main"
+        `}
+        gap={'4'}
+      >
+        <GridItem area={'title'}>
+          <Text fontSize="3xl" fontWeight="extrabold" alignSelf={'flex-start'}>Stock Management</Text>
+        </GridItem>
+        <GridItem area={'main'}>
+          <Content/>
+        </GridItem>
+      </Grid>
+    </ContextProvider>
+
+  );
 };
 
 export default Management;
